62fc48792e861c52a51c22f014c292d35b87f107
Balti <baltiboix@gmail.com>
2023-03-04 09:32:47 +0100

    Update dueling

--- tankEnvV2.py
+++ tankEnvV2.py
@@ -60,13 +60,6 @@ def randomAssay(nCrudes=NC, nCuts=NCUTS):
         for i in range(nCrudes):
             a.append(randomComp(nCuts))
     return np.transpose(np.array(a))
-def flatten_dict(dd, separator='_', prefix=''):
-    '''https://stackoverflow.com/questions/6027558/flatten-nested-dictionaries-compressing-keys'''
-    return { prefix + separator + k if prefix else k : v
-             for kk, vv in dd.items()
-             for k, v in flatten_dict(vv, separator, kk).items()
-             } if isinstance(dd, dict) else { prefix : dd }
-
 
 class crudeParcel():
     def __init__(self, vol: float, comp: np.array):
